409f9caf79fdab9873397e7a09d13c6be97e13c5
milton <mitnunes.pro.com>
2025-08-19 16:36:47 -0300

    add1

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  const adder = (num) => {
+    if (num === undefined) {
+      const result = sum;
+      sum = 0;
+      return result;
+    }
+
+    if (typeof num !== 'number') {
+      throw new TypeError('Argument must be a number');
+    }
+
+    sum += num;
+    return adder;
+  }
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
